1a1279f9513e2c770e0be21d8465ab164cd859eb
Pixality-Studio <>
2019-04-28 22:35:28 -0400

    Object Update
    - Patched the Vending Machine so you can add required coin amounts.
    - Machines also have a label to display how many coins are needed
    
    - Key created with comments
    - Door created with comments

--- Raea/VendingMachine.gd
+++ Raea/VendingMachine.gd
@@ -1,24 +1,35 @@
 extends Area2D
 
-export (PackedScene) var key
+export (PackedScene) var scene
 export (int, "Body", "Slot") var collision_type #This will tell the game what type of coin collision will spawn a key
+export (NodePath) var connected_to #This is used ONLY if this spawns a key that needs to be connected to a door
+export (int) var coins_needed
 
 func _ready():
 	if collision_type == 0: #If it's a body hit that counts
 		self.connect("body_entered", self, "vending_machine_hit") #Connect respective collider to the function
 	if collision_type == 1: #If it's a coin slot hit that counts
 		$CoinSlot.connect("body_entered", self, "vending_machine_hit")
+	
+	$RequiredCoinLabel.text = str(coins_needed)
 
 #This function handles body collisions
 func vending_machine_hit(body):
 	if body.is_in_group("coins"): #If the body is a coin
 		body.queue_free() #Destroy the coin
-		spawn_key() #Spawn a key
+		coins_needed -= 1
+		$RequiredCoinLabel.text = str(coins_needed)
+		
+		if coins_needed == 0:
+			spawn_scene() #Spawn a key
 
 #This function spawns in a key
-func spawn_key():
-	var k = key.instance()
+func spawn_scene():
+	var k = scene.instance()
+	if connected_to != null:
+		k.connect("unlock_door", get_node(connected_to), "unlock")
 	add_child(k)
 	k.position = $KeySpawn.position
+	print("SPAWN")
 
 
