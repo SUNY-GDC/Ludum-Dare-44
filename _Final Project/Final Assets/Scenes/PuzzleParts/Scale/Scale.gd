5d83c4f8c82c78b3c0adc6ff2ea8d91415df5b1d
dremaster007 <matosman1209@gmail.com>
2019-04-29 16:25:15 -0400

    Revert "Made most of the Main scene's functionality. Need levels"
    This reverts commit dc3b5107d199cf16ca9eff90a018c0c9d5b099f6.

--- _Final Project/Final Assets/Scenes/PuzzleParts/Scale/Scale.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _physics_process(delta):
-	pass
-	#print($ScaleTipper.rotation)
-
-#How it works:
-#When more weight is placed on a side the beam will tilt to that side. Basic physics.
-#To adjust the tilt max of the scale simply adjust the length of ScaleTipper and the height of TiltMiddle
